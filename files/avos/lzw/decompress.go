6f5d79ede2049b5540fb4ba4f341b6b88a09ec9f
ddascalescu <djdascalescu@gmail.com>
2023-01-16 03:47:42 +0000

    CODE: Handles odd-codes edge case

--- files/avos/lzw/decompress.go
+++ files/avos/lzw/decompress.go
@@ -7,7 +7,7 @@ func Decompress(data []uint8) []uint8 {
 
 	fmt.Println()
 	nBytes := len(data)
-	fmt.Printf("%v bytes\n", nBytes)
+	fmt.Printf("%v uint8s\n", nBytes)
 	codes := []uint16{}
 	for i := 0; i < nBytes; i += 3 {
 		if i < nBytes-2 {
@@ -17,10 +17,18 @@ func Decompress(data []uint8) []uint8 {
 			var code2 uint16 = (uint16(byte2) & 0b00001111) << 8 | uint16(byte3)
 			codes = append(codes, code1, code2)
 		} else {
-			// TODO: handle edge case
+			if i == nBytes-1 {
+				fmt.Println("ERROR: Last code only has 8 bits out of 12.")
+				continue
+			}
+
+			var byte1, byte2 uint8 = data[i], data[i+1]
+			
+			var code uint16 = (uint16(byte1) << 8) | uint16(byte2)
+			codes = append(codes, code)
 		}
 	}
-	fmt.Println(codes[:4])
+	fmt.Printf("%v uint16s\n", len(codes))
 
 	fmt.Println("Done.")
 	return nil
